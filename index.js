5a503da8caae2510c810d7f29764ed6b94db9781
Filippo Baldi <baldi.filippo@icloud.com>
2022-07-12 12:02:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ ScrollReveal().reveal('footer', { delay: 100 })
 ScrollReveal().reveal('.title', slideUp)
 ScrollReveal().reveal('.subtitle', { delay: 600 })
 ScrollReveal().reveal('.card-image', { delay: 100 })
+
+const returnTopBtn = document.querySelector('.return-top-button')
+
+//make it visible only when you scroll down
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 250) {
+    returnTopBtn.classList.add('visible')
+  } else {
+    returnTopBtn.classList.remove('visible')
+  }
+})
+
+returnTopBtn.addEventListener('click', () => {
+  returnTopBtn.classList.remove('visible')
+
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+})
